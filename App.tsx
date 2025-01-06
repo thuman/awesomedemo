d1afcda61b393c755e49544f2ddcdfb7c028d1c5
human <humanhuman@global.com>
2025-01-06 21:32:45 +0800

    update: measureInWindow demo

--- App.tsx
+++ App.tsx
@@ -1,117 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState, useRef } from 'react';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const viewRef = useRef(null);
+  const [viewPosition, setViewPosition] = useState({ x: 0, y: 0 });
+  let timer = null
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const onScroll = () => {
+    console.log('onScroll')
+    if (viewRef && viewRef.current) {
+      try {
+        viewRef.current.measureInWindow(function (x, y, width, height) {
+          console.log({x, y, width, height})
+          setViewPosition({ x: x, y: y });
+        });
+      } catch (error) {
+        console.error('Error measuring the view:', error);
+      }
+    }
   };
-
+  
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <ScrollView onScroll={onScroll}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Scroll the screen</Text>
+        <View ref={viewRef} style={styles.measuredView}>
+          <Text style={styles.text}>This is the view to be measured x:{viewPosition.x} y:{viewPosition.y} </Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        <View style={styles.bottom}></View>
+      </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  bottom: {
+    height: 700,
+    width: 100,
+    backgroundColor: 'red'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  text: {
+    fontSize: 20,
+    margin: 20,
   },
-  highlight: {
-    fontWeight: '700',
+  measuredView: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'lightblue',
+    margin: 20,
   },
 });
 
